78f104497e6a6b2759a419bd39fadbb2c5883e43
zaenhakeem <74319261+zaenhakeem@users.noreply.github.com>
2022-04-06 23:29:44 +0300

    Add files via upload

---
+++ Election/Course selection/Course selection/Form1.Designer.vb
@@ -0,0 +1,127 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.cmdRating = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.txtScore = New System.Windows.Forms.TextBox()
+        Me.txtRating = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(33, 43)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(100, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Stud Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(33, 91)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(67, 25)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Course"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(33, 139)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(56, 25)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Score"
+        '
+        'cmdRating
+        '
+        Me.cmdRating.Location = New System.Drawing.Point(33, 193)
+        Me.cmdRating.Name = "cmdRating"
+        Me.cmdRating.Size = New System.Drawing.Size(112, 34)
+        Me.cmdRating.TabIndex = 3
+        Me.cmdRating.Text = "Rating"
+        Me.cmdRating.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(237, 43)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(150, 31)
+        Me.TextBox1.TabIndex = 4
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(237, 91)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(150, 31)
+        Me.TextBox2.TabIndex = 5
+        '
+        'txtScore
+        '
+        Me.txtScore.Location = New System.Drawing.Point(237, 139)
+        Me.txtScore.Name = "txtScore"
+        Me.txtScore.Size = New System.Drawing.Size(150, 31)
+        Me.txtScore.TabIndex = 6
+        '
+        'txtRating
+        '
+        Me.txtRating.Location = New System.Drawing.Point(237, 193)
+        Me.txtRating.Name = "txtRating"
+        Me.txtRating.Size = New System.Drawing.Size(150, 31)
+        Me.txtRating.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1256, 547)
+        Me.Controls.Add(Me.txtRating)
+        Me.Controls.Add(Me.txtScore)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.cmdRating)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents cmdRating As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents txtScore As TextBox
+    Friend WithEvents txtRating As TextBox
+End Class
---
+++ Election/Course selection/Course selection/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub cmdRating_Click(sender As Object, e As EventArgs) Handles cmdRating.Click
+
+        Dim Score As Integer
+        Score = txtScore.Text
+        Select Case Score
+            Case 0 To 49
+                txtRating.Text = "Below Average"
+            Case 50 To 59
+                txtRating.Text = "Above Average"
+            Case 60 To 69
+                txtRating.Text = "Good Performance"
+            Case Else
+                txtRating.Text = "Distinction"
+
+        End Select
+
+    End Sub
+End Class
